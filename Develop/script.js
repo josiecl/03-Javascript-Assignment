00c7bcbb157ad6a408a8b0070fab23c05c268f4e
Josie <josiecollinsrc@gmail.com>
2021-01-06 17:52:19 -0600

    work done in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,30 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+//generator for password
+function generate() {
+  let complexity = document.getElementById("generate").value;
+
+  //possible password values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=";
+
+  let password = "";
+
+  //create for loop to choose password characters
+  for(var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  document.getElementById("display").value = password;
 }
 
 // Add event listener to generate button
