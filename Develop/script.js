86f47db9a2d7312b0d2630666975eefe2bf9cfe9
Josie <josiecollinsrc@gmail.com>
2021-01-16 09:45:04 -0600

    additions to function/changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays for the prompts
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var capitals = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var undercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var symbolic = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "?"];
+
 // Write password to the #password input
 function writePassword() {
 
@@ -13,32 +19,134 @@ function writePassword() {
 }
 
 
-//generator for password
+
+
+//generator for 
+
+// function getRandom(array) {
+//   var randomIndex = Math.floor(Math.random() * array.length);
+//   var randomCharacter = array[randomIndex];
+//   console.log(randomCharacter);
+//   return randomCharacter;
+// }
+
+// Function to generate the password
 function generatePassword() {
-  let complexity = document.getElementById("generate").value;
+  var options = getOptions();
+  var possibleCharacters = [];
+  var result = [];
 
-  var length = prompt("Between 8 to 128, how many characters?");
-  if (length < 8) {
-    alert("Error");
+  
+ 
+
+  if (options.uppercase) {
+    for(var i = 0; i < capitals.length; i++) {
+      possibleCharacters.push(capitals[i]);
+    }
+
+  };
+
+  if (options.lowercase) {
+    for(var i = 0; i < undercase.length; i++) {
+      possibleCharacters.push(undercase[i]);
+    }
+
+  };
+
+  if (options.numbers) {
+    for(var i = 0; i < numeric.length; i++) {
+      possibleCharacters.push(numeric[i]);
+    }
+    // console.log(possibleCharacters);
+  };
+
+  if (options.symbols) {
+    for(var i = 0; i < symbolic.length; i++) {
+      possibleCharacters.push(symbolic[i]);
+    }
+
   }
-  if (length > 128) {
-    alert("Error");
+
+  var newPassword = "";
+
+  for(var i = 0; i < options.length; i++) {
+
+    newPassword += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+    // console.log(newPassword);
+
+
+    // var possibleCharacter = getRandom(possibleCharacters);
+    // result.push(possibleCharacter);
+    // console.log(result);
   }
 
-  //possible password values
-  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=";
+  return newPassword
 
-  let password = "";
+  
+
 
   //create for loop to choose password characters
-  for(var i = 0; i <= length; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
-    console.log(password);
-  }
 
-  return(password);
+  // for(var i = 0; i <= length; i++){
+  //   password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
+  //   console.log(password);
+  // }
+
+  // return(password);
+
   // document.getElementById("returnPassword").textContent = password;
 }
+
+
+
+
+
+// function to ask all the prompt questions, then retrieve selected answers and push them to the output
+function getOptions() {
+
+  // Running into issues if length < 8 and if length is near 128.. 
+  var length = prompt("Between 8 to 128, how many characters?");
+  if (length < 8) {
+    alert("Error: must be at least 8 characters");
+    getOptions();
+  }
+  if (length > 128) {
+    alert("Error: must be at most 128 characters");
+    getOptions();
+  }
+
+
+  // Variables for the prompts
+  var uppercase = confirm("Use uppercase letters?")
+  var lowercase = confirm("Use lowercase letters?")
+  var numbers = confirm("Use numbers?")
+  var symbols = confirm("Use the following symbols: !@#$%^&*()-_=?")
+
+  // Tell the user that they must use at least one prompt
+  if(uppercase === false && symbols === false && numbers === false && lowercase === false) {
+    alert("Must choose at least one parameter");
+    getOptions();
+  }
+  else {
+    var passwordOptions = {
+      length: length, 
+      uppercase: uppercase,
+      lowercase: lowercase,
+      numbers: numbers,
+      symbols: symbols
+    }
+
+    return passwordOptions
+  }
+
+
+
+
+  
+}
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 03-Javascript-Assignment
 Third assignment, random password generator
+
+# Why it was made
+It's important to have security in a password. Passwords are used on so many sites, and reusing a password, or using a personal one, can put one's information at risk. This random password generator gives users a randomly generated password that allows them to choose what values are used in it, so it can be somewhat personalized. Random passwords are usually much more difficult to guess than a personal password, which gives an extra layer of security as well.
+
+# How it works
+Upon pressing the 'generate password' button, the user will be greeted with several prompts. If the user clicks 'no' for all of them, then the prompts will alert the user that they must select at least one.
+
+# Issues
+While working on this code, I ran into several issues. My biggest one was that I kept getting an 'error: out of memory' alert. I had inadvertently written a 'for' loop incorrectly, so it kept running until the webpage would run out of memory and crash. Thankfully, this was fixed relatively easily, but it did set me back for a while.
+I also kept getting a console log error. I was unsure how to fix this (and still am, rip)
